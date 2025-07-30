e1ebdc711db78f2044e8af4e9ddf340732a0ef7b
Lars Kruse <lakr@thetechcollective.eu>
2025-07-30 14:20:59 +0000

    mark flows pending – resolves #9
    e3c9ecb: multiline input - #9
    
    cf31aec: name change - #9
    
    971a55b: ready to experiment - #9
    
    f20f840: ready to roll - #9
    
    166f2d2: update names - #9
    
    2c44bdd: update names - #9
    
    322b996: set the pending state - #9

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -17,7 +17,7 @@ if [ -e $(dirname $0)/_temp.token ]; then
     echo "$PREFIX setting up GitHub CLI"
     $(dirname $0)/gh-login.sh postcreate
     echo "$PREFIX Installing the techcollective/gh-tt gh cli extension"
-    gh extension install thetechcollective/gh-tt
+    gh extension install thetechcollective/gh-tt --pin experimental
     echo "$PREFIX Installing the gh aliases"    
     gh alias import .devcontainer/.gh_alias.yml --clobber
 fi
